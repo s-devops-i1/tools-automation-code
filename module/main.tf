b8826f0a2f4007066de816f2a23d9fdbeafa0af2
shuja <shuja.hafez@gmail.com>
2024-06-03 18:17:21 +0530

    created insance profile

--- module/main.tf
+++ module/main.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "instance" {
-  ami           = data.aws_ami.ami.image_id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
+#   iam_instance_profile   =
+
   tags = {
     Name = var.tool_name
   }
@@ -36,4 +38,9 @@ resource "aws_iam_role" "test_role" {
   tags = {
     Name = "${var.tool_name}-role"
   }
+}
+
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "${var.tool_name}-profile"
+  role = aws_iam_role.test_role.name
 }

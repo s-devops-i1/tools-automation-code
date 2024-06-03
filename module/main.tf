91a9e42dbda2150d593f11f7ab85873e0e392c7c
shuja <shuja.hafez@gmail.com>
2024-06-03 18:03:56 +0530

    iam role created for prometheus

--- module/main.tf
+++ module/main.tf
@@ -16,3 +16,24 @@ resource "aws_route53_record" "record" {
   ttl     = 30
 }
 
+resource "aws_iam_role" "test_role" {
+  name = "${var.tool_name}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    Name = "${var.tool_name}-role"
+  }
+}

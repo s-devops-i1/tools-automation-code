3cddbaf07250ef87365ae0498907915305a7008b
shuja <shuja.hafez@gmail.com>
2024-06-06 09:47:20 +0530

    grafana instance added

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "tool" {
   source = "./module"
-  for_each = var.instance_type
+  for_each = var.tools
 
-  tool_name             = var.tool_name
+  tool_name             = each.key
   instance_type         = each.value["instance_type"]
   zone_id               = var.zone_id
   policy-resource-list  = each.value["policy-resource-list"]
--- variables.tf
+++ variables.tf
@@ -1,14 +1,16 @@
-variable "tool_name" {
-  default = "Prometheus"
-}
-variable "zone_id" {
-  default = "Z10377495CKDE7OXJB1E"
-}
-variable "instance_type" {
+variable "tools" {
   default = {
     prometheus = {
-      instance_type         = "t3.micro"
-      policy-resource-list  = ["ec2:DescribeInstances"]
+      instance_type        = "t3.micro"
+      policy-resource-list = ["ec2:DescribeInstances"]
+    }
+    grafana = {
+      instance_type        = "t3.micro"
+      policy-resource-list = ["ec2:DescribeInstances"]
+
     }
   }
 }
+variable "zone_id" {
+   default = "Z10377495CKDE7OXJB1E"
+  }

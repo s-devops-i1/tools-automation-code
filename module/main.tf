772328bbf8f437438d171b3bdc2e9fa2a08eb9ff
shuja <shuja.hafez@gmail.com>
2024-06-06 10:33:14 +0530

    internal dns record added

--- module/main.tf
+++ module/main.tf
@@ -24,8 +24,15 @@ resource "aws_route53_record" "record" {
   name    = var.tool_name
   type    = "A"
   zone_id = var.zone_id
+  records = [aws_instance.instance.public_ip]
+  ttl     = 3
+}
+resource "aws_route53_record" "record-internal" {
+  name    = "${var.tool_name}-internal"
+  type    = "A"
+  zone_id = var.zone_id
   records = [aws_instance.instance.private_ip]
-  ttl     = 30
+  ttl     = 3
 }
 
 resource "aws_iam_role" "test_role" {

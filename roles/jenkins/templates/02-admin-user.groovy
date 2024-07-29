abfe9391e8e2f58f1657d0ec88fe8c6be8b840d3
shuja <shuja.hafez@gmail.com>
2024-07-29 23:32:21 +0530

    init scripts replaced with sir code

--- roles/jenkins/templates/02-admin-user.groovy
+++ roles/jenkins/templates/02-admin-user.groovy
@@ -10,7 +10,7 @@ println "--> creating admin user"
 //def adminPassword = System.getenv("ADMIN_PASSWORD")
 
 def adminUsername = "{{secrets.jenkins_user}}"
-def adminPassword = "{{{secrets.jenkins_password}}"
+def adminPassword = "{{secrets.jenkins_password}}"
 
 assert adminPassword != null : "No ADMIN_USERNAME env var provided, but required"
 assert adminPassword != null : "No ADMIN_PASSWORD env var provided, but required"

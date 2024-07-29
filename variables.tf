b395790dc2dea9f727a45ac77e432bedd3bd64e7
shuja <shuja.hafez@gmail.com>
2024-07-29 15:24:28 +0530

    Creating jenkins server

--- variables.tf
+++ variables.tf
@@ -1,31 +1,36 @@
 variable "tools" {
   default = {
-    prometheus = {
-      instance_type        = "t3.micro"
-      policy-resource-list = ["ec2:DescribeInstances"]
-    }
-    grafana = {
-      instance_type        = "t3.micro"
-      policy-resource-list = []
-
-    }
+    #     prometheus = {
+    #       instance_type        = "t3.micro"
+    #       policy-resource-list = ["ec2:DescribeInstances"]
+    #     }
+    #     grafana = {
+    #       instance_type        = "t3.micro"
+    #       policy-resource-list = []
+    #
+    #     }
     vault = {
       instance_type        = "t3.micro"
       policy-resource-list = []
 
     }
-    elk = {
-      instance_type        = "r7i.large"
-      policy-resource-list = []
-    }
+    #     elk = {
+    #       instance_type        = "r7i.large"
+    #       policy-resource-list = []
+    #     }
     ci-server = {
       instance_type        = "t3.small"
       policy-resource-list = []
     }
-    SonarQube = {
-      instance_type        = "t3.large"
+    Jenkins = {
+      instance_type        = "t3.small"
       policy-resource-list = []
     }
+    #     SonarQube = {
+    #       instance_type        = "t3.large"
+    #       policy-resource-list = []
+    #     }
+    #   }
   }
 }
 variable "zone_id" {
